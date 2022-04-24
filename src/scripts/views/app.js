1252fd224303c520ee0a6693a49615c28098e904
Diandara TResya Haniva <diandaraa@gmail.com>
2022-04-24 21:27:25 +0700

    konfigurasi drawer

---
+++ src/scripts/views/app.js
@@ -0,0 +1,23 @@
+import DrawerInitiator from '../utils/drawer-initiator';
+// Class App dibuat bertujuan untuk menginisiasikan komponen-komponen dari Application Shell
+class App {
+    constructor({ button, drawer, content }) {
+      this._button = button;
+      this._drawer = drawer;
+      this._content = content;
+
+      this._initialAppShell(); // untuk menginisiasikan komponen-komponen AppShell yang sudah dimasukkan pada properti class
+    }
+
+    _initialAppShell() {
+        // TODO: initial Drawer
+        // kita bisa menginisiasikan komponen lain bila ada
+        DrawerInitiator.init({
+            button: this._button,
+            drawer: this._drawer,
+            content: this._content,
+      });
+  }
+}
+
+  export default App;
