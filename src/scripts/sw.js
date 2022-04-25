63324a7422c32fa4a31e8d20d96a8cd595502da3
Diandara TResya Haniva <diandaraa@gmail.com>
2022-04-25 20:53:33 +0700

    Menerapkan Cache pada Service Worker

--- src/scripts/sw.js
+++ src/scripts/sw.js
@@ -1,18 +1,19 @@
+import 'regenerator-runtime';
+import CacheHelper from './utils/cache-helper';
+
+const { assets } = global.serviceWorkerOption; //untuk mendapatkan seluruh aset yang membentuk keseluruhan aplikasi (app shell). 
+
 self.addEventListener('install', (event) => {
-    console.log('Installing Service Worker ...');
-   
-    // TODO: Caching App Shell Resource
+    //Caching App Shell Resource
+    event.waitUntil(CacheHelper.cachingAppShell([...assets, './']));
   });
    
   self.addEventListener('activate', (event) => {
-    console.log('Activating Service Worker ...');
-   
-    // TODO: Delete old caches
+    // Delete old caches
+    event.waitUntil(CacheHelper.deleteOldCache());
   });
    
   self.addEventListener('fetch', (event) => {
-    console.log(event.request);
-   
+    //Add/get fetch request to/from caches
     event.respondWith(fetch(event.request));
-    // TODO: Add/get fetch request to/from caches
   });
