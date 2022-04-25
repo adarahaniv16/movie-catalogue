43db8c8045da61fc5f5d551bdd564d9dd414a487
Diandara TResya Haniva <diandaraa@gmail.com>
2022-04-25 16:49:35 +0700

    melakukan request dari API

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: '21108b07b2a54d223edb4ca7f589d722',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
--- src/scripts/views/pages/now-playing.js
+++ src/scripts/views/pages/now-playing.js
@@ -1,13 +1,19 @@
+import TheMovieDbSource from '../../data/themoviedb-source';
+ 
 const NowPlaying = {
-    async render() {
-      return `
-        <h2>Now Playing Page</h2>
-      `;
-    },
-   
-    async afterRender() {
-      // Fungsi ini akan dipanggil setelah render()
-    },
-  };
-   
-  export default NowPlaying;
+  async render() {
+    return `
+      <h2>Now Playing Page</h2>
+    `;
+  },
+ 
+  async afterRender() {
+    //fumgsi ini akan dipanggil setelah render
+    const movies = await TheMovieDbSource.nowPlayingMovies();
+    console.log(movies);
+ 
+    // TODO: tampilkan movies di dalam DOM
+  },
+};
+ 
+export default NowPlaying;
